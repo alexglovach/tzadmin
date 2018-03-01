1c7e6718904d901276485be99b9c30d308a4932b
alexglovach <alexglovach@gmail.com>
2018-03-01 18:37:03 +0200

    add to github

--- config/services.php
+++ config/services.php
@@ -6,7 +6,7 @@
             $pdo = new PDO($config["driver"] . ':host=' . $config["host"] . ';dbname=' . $config["database"],
                 $config["username"],
                 $config["password"]);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $pdo;
         }
         catch(PDOException $e)
